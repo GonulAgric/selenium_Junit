5ef9dc0bcb0f4bf205b8b3e7f9e5c5653b2bba1e
Gonul Agric <agricyakup@gmail.com>
2023-04-06 15:21:35 +0300

    day13_selenium

---
+++ src/test/java/day13_ActionsClass/C01_ActionsClass.java
@@ -0,0 +1,35 @@
+package day13_ActionsClass;
+
+import Utilities.TestBase;
+import org.junit.Assert;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+
+import java.awt.*;
+
+public class C01_ActionsClass extends TestBase {
+
+    /*
+    https://the-internet.herokuapp.com/context_menu  sitesine gidin
+    Kutuya sağ tıklayın
+    Alert'te cikan yazinin "You selected a context menu" oldugunu test edin
+    Tamam diyerek alert'i kapatın
+ */
+    @Test
+    public void name() {
+
+//        https://the-internet.herokuapp.com/context_menu  sitesine gidin
+        driver.get(" https://the-internet.herokuapp.com/context_menu");
+//        Kutuya sağ tıklayın
+        WebElement kutu= driver.findElement(By.id("hot-spot"));
+        Actions actions=new Actions(driver);
+        actions.contextClick(kutu).perform();
+//        Alert'te cikan yazinin "You selected a context menu" oldugunu test edin
+       String alert= driver.switchTo().alert().getText();
+        Assert.assertEquals( "You selected a context menu",alert);
+//        Tamam diyerek alert'i kapatın
+        driver.switchTo().alert().accept();
+    }
+}
---
+++ src/test/java/day13_ActionsClass/C02_ActionClass.java
@@ -0,0 +1,34 @@
+package day13_ActionsClass;
+
+import Utilities.TestBase;
+import org.junit.Assert;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Action;
+import org.openqa.selenium.interactions.Actions;
+
+public class C02_ActionClass extends TestBase {
+
+    /*
+    Amazon anasayfasına gidin
+    "Account & List" üzerinde bekleyin (mouse over - hover over)
+    Açılan menüde "Account" linkine tıklayın
+    Açılan sayfanın başlığının "Your Account" içerdiğini doğrulayın
+     */
+
+    @Test
+    public void name() {
+//
+//        Amazon anasayfasına gidin
+        driver.get("https://amazon.com");
+//        "Account & List" üzerinde bekleyin (mouse over - hover over)
+        WebElement accountList=driver.findElement(By.xpath("//span[@class='nav-line-2 ']"));
+        Actions action=new Actions(driver);
+        action.moveToElement(accountList).perform();//moveToElement() methodu "hover over işlemini gerceklestirir.
+//        Açılan menüde "Account" linkine tıklayın
+         driver.findElement(By.linkText("Account")).click();
+//        Açılan sayfanın başlığının "Your Account" içerdiğini doğrulayın
+        Assert.assertTrue(driver.getTitle().contains("Your Account"));
+    }
+}
